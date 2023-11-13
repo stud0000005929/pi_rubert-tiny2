083e212aaacf9187634fd9ea53f51abb8ebfb6be
stud0000005929 <146352807+stud0000005929@users.noreply.github.com>
2023-11-13 19:33:37 +0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,2 @@
 import streamlit as st
-from transformers import pipeline
-model = pipeline(model="seara/rubert-tiny2-russian-sentiment")
-model("Привет, ты мне нравишься!")
 st.text("Привет, ты мне нравишься!")
